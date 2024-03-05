a7d15838dab8fc8dbf02551b09627a2e980ba9ba
programanic <statego@programanic.com>
2024-03-05 17:34:49 +0900

    init

---
+++ sample/state/TestControl.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+class TestControl
+{
+public:
+    void main();
+};
---
+++ sample/test1.cpp
@@ -0,0 +1,14 @@
+﻿// test1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "state/TestControl.hpp"
+
+int main()
+{
+	TestControl tc;
+	tc.main();
+	return 0;
+}
